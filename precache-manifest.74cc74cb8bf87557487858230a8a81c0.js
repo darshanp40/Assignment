9ce5618a5499b19e75f22eba1bd3253e4582b057
Darshan Pawar <darshan.pawar@DarshanPawar1.local>
2018-12-02 18:53:36 +0530

    Added build folder

---
+++ precache-manifest.74cc74cb8bf87557487858230a8a81c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "916966ba36cd65402689",
+    "url": "/static/js/main.916966ba.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "916966ba36cd65402689",
+    "url": "/static/css/main.a22c3973.chunk.css"
+  },
+  {
+    "revision": "9d03d1bcd62d5539cf733dcc5b3a90d8",
+    "url": "/index.html"
+  }
+];
